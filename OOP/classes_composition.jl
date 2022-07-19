ad4014c46066b337f027d04787b1cfce17444430
MiguelMarcelino <mac.marcelino@outlook.pt>
2022-07-19 15:56:12 +0100

    Updates class translation

--- OOP/classes_composition.jl
+++ OOP/classes_composition.jl
@@ -1,9 +1,10 @@
 abstract type AbstractFoo end
+abstract type AbstractShape end
+abstract type AbstractSquare <: AbstractShape end
 abstract type AbstractPerson end
 abstract type AbstractStudent <: AbstractPerson end
 abstract type AbstractWorker <: AbstractPerson end
 mutable struct Foo <: AbstractFoo
-
 end
 function bar(self::AbstractFoo)::Int64
     return baz(self)
@@ -17,11 +18,32 @@ function bar_str(self::AbstractFoo)::String
     return "a"
 end
 
+mutable struct Shape <: AbstractShape
+    x
+    y
+end
+function position(self::AbstractShape)
+    return "($(self.x), $(self.y))"
+end
+
+mutable struct Square <: AbstractSquare
+    #= Two-dimensional square =#
+    x
+    y
+    side
+    Square(x, y, side) = begin
+        Shape(x, y)
+        new(x, y, side)
+    end
+end
+function area(self::AbstractSquare)
+    self.x * self.y
+end
+
 mutable struct Person <: AbstractPerson
     name::String
 end
-
-function get_name(self::AbstractPerson)::String
+function get_id(self::AbstractPerson)::String
     return self.name
 end
 
@@ -29,28 +51,17 @@ mutable struct Student <: AbstractStudent
     name::String
     student_number::Int64
     domain::String
-
     Student(name::String, student_number::Int64, domain::String = "school.student.pt") =
-        begin
-            # if student_number < 0
-            #     throw(ValueError("Student number must be a positive number"))
-            # end
-            new(name, student_number, domain)
-        end
+        new(name, student_number, domain)
 end
-
-function get_name(self::AbstractStudent)
+function get_id(self::AbstractStudent)
     return "$(self.name) - $(self.student_number)"
 end
 
 mutable struct Worker <: AbstractWorker
+    name::String
     company_name::String
     hours_per_week::Int64
-
-    Worker(name::String, company_name::String, hours_per_week::Int64) = begin
-        Person(name)
-        new(name, company_name, hours_per_week)
-    end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
@@ -59,9 +70,21 @@ if abspath(PROGRAM_FILE) == @__FILE__
     @assert(b == 10)
     c = bar_str(f)
     @assert(c == "a")
+    shape = Shape(1, 3)
+    square = Square(2, 4, 5)
+    @assert(position(square) == "(2, 4)")
     p = Person("P")
+    @assert(p.name == "P")
+    @assert(get_id(p) == "P")
     s = Student("S", 111111)
-    @assert(get_name(p) == "P")
-    @assert(get_name(s) == "S - 111111")
+    @assert(s.name == "S")
+    @assert(s.student_number == 111111)
+    @assert(s.domain == "school.student.pt")
+    @assert(get_id(s) == "S - 111111")
+    w = Worker("John", "Siemens", 35)
+    @assert(w.name == "John")
+    @assert(w.company_name == "Siemens")
+    @assert(w.hours_per_week == 35)
+    @assert(get_id(w) == "John")
     println("OK")
 end
--- OOP/classes_oop.py
+++ OOP/classes_oop.py
@@ -1,5 +1,6 @@
-# @jl_class # For PyJL
 # Simple class translation
+
+@jl_class # For PyJL
 class Foo:
     def bar(self):
         return self.baz()
@@ -10,21 +11,41 @@ class Foo:
     def bar_str(self):
         return "a"
 
+@jl_class # For PyJL
+class Shape():
+    def __init__(self, x, y):
+        # Center on a 2-dimensional plane
+        self.x = x
+        self.y = y
+
+    def position(self):
+        return f"({self.x}, {self.y})"
+
+@jl_class # For PyJL 
+class Square(Shape):
+    """Two-dimensional square"""
+    def __init__(self, x, y, side):
+        super().__init__(x, y)
+        self.side = side
+
+    def area(self):
+        self.x * self.y
+
 
 ######################################
 ### Example of the Diamond Problem ###
 ######################################
 
-# @jl_class # For PyJL
+@jl_class # For PyJL
 class Person:
     def __init__(self, name: str):
         self.name = name
 
-    def get_name(self):
+    def get_id(self):
         return self.name
 
 
-# @jl_class # For PyJL
+@jl_class # For PyJL
 class Student(Person):
     def __init__(
         self, name: str, student_number: int, domain: str = "school.student.pt"
@@ -35,46 +56,68 @@ class Student(Person):
         self.student_number = student_number
         self.domain = domain
 
-    def get_name(self):
+    def get_id(self):
         return f"{self.name} - {self.student_number}"
 
-
+@jl_class # For PyJL
 class Worker(Person):
     def __init__(
         self, name: str, company_name: str, hours_per_week: int
     ):
-        Person.__init__(self, name)
+        self.name = name
         self.company_name = company_name
         self.hours_per_week = hours_per_week
 
 
-class StudentWorker(Student, Worker):
-    def __init__(self, name: str, student_number: int, domain: str,
-            company_name: str, hours_per_week: int, is_exhausted:bool):
-        Student.__init__(self, name, student_number, domain)
-        Worker.__init__(self, name, company_name, hours_per_week)
-        self.is_exhausted = is_exhausted
-
+# class StudentWorker(Student, Worker):
+#     def __init__(self, name: str, student_number: int, domain: str,
+#             company_name: str, hours_per_week: int, is_exhausted:bool):
+#         Student.__init__(self, name, student_number, domain)
+#         Worker.__init__(self, name, company_name, hours_per_week)
+#         self.is_exhausted = is_exhausted
 
 
 if __name__ == "__main__":
-    # Accessing class values
+    # Accessing class values and methods
     f = Foo()
     b = f.bar()
     assert b == 10
     c = f.bar_str()
     assert c == "a"
 
-    #  Test inheritance
+    shape = Shape(1, 3)
+    square = Square(2, 4, 5)
+    assert square.position() == "(2, 4)"
+
+    # Test inheritance
     p = Person("P")
+    assert p.name == "P"
+    assert p.get_id() == "P"
+
     s = Student("S", 111111)
-    assert p.get_name() == "P"
-    assert s.get_name() == "S - 111111"
+    assert s.name == "S"
+    assert s.student_number == 111111
+    assert s.domain == "school.student.pt"
+    assert s.get_id() == "S - 111111"
+
+    w = Worker("John", "Siemens", 35)
+    assert w.name == "John"
+    assert w.company_name == "Siemens"
+    assert w.hours_per_week == 35
+    assert w.get_id() == "John"
 
     # Exceptions
-    # Student("S2", -1) # Should raise an exception
+    # Student("S2", -1) # Raises an exception if uncommented
 
-    sw = StudentWorker("Timo Marcello", 1111, "school.student.pt",
-        "Cisco", 40, True)
-    print(sw.company_name)
+    # Multiple inheritance
+    # sw = StudentWorker("Timo Marcello", 1111, "school.student.pt",
+    #     "Cisco", 40, True)
+    # assert sw.company_name == "Cisco"
+    # assert sw.is_exhausted == True
+    # assert sw.name == "Timo Marcello"
+    # assert sw.student_number == 1111
+    # assert sw.domain == "school.student.pt"
+    # assert sw.company_name == "Cisco"
+    # assert sw.hours_per_week == 40
+    # assert sw.is_exhausted == True
     print("OK")
